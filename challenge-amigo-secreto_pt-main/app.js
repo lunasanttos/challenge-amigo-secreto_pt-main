0e26be7f73cc1ef3cb531c3d4e11ca47d221369c
luna santos <oluna8466@gmail.com>
2025-03-18 14:20:45 -0300

    Validando se o nome ta duplicado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -2,11 +2,17 @@ let amigos = [];
 
 function adicionarAmigo() {
     const inputAmigo = document.getElementById('amigo');
-    const nomeAmigo = inputAmigo.value.trim();
+    const nomeDoAmigo = inputAmigo.value.trim();
 
     
-    if (nomeAmigo === "") {
+    if (nomeDoAmigo === "") {
         alert("Por favor, insira um nome.");
         return; 
     }
+  
+    
+    if (amigos.includes(nomeDoAmigo)) {
+      alert(`O nome ${nomeDoAmigo} já está na lista.`);
+      return;
+   }
   }
