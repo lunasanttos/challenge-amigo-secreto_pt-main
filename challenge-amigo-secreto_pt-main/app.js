1461e5b6774313db283cb29a74bd858348b8a679
luna santos <oluna8466@gmail.com>
2025-03-18 14:18:34 -0300

    Adicionando amigo na lista e verificando se o campo está vazio

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,12 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+
+    
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return; 
+    }
+  }
