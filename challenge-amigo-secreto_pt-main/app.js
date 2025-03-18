da540bca526cb17b13e57bb7d475669988465ee7
luna santos <oluna8466@gmail.com>
2025-03-18 14:23:46 -0300

    Atualizando a lista na interface

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -15,4 +15,23 @@ function adicionarAmigo() {
       alert(`O nome ${nomeDoAmigo} já está na lista.`);
       return;
    }
+
+   amigos.push(nomeDoAmigo);
+   
+   inputAmigo.value = "";
+
+   atualizarLista();
   }
+
+  
+function atualizarLista() {
+  const listaDeAmigos = document.getElementById('listaDeAmigos');
+  
+  listaDeAmigos.innerHTML = ""; 
+
+  for (let i = 0; i < amigos.length; i++) {
+      const li = document.createElement('li');
+      li.textContent = amigos[i];
+      listaAmigos.appendChild(li);
+  }
+}
